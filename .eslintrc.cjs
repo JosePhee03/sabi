1b1adcb6de5e2fb3ff91bf5c799d4924687515ed
JosePhee03 <tyttocr@gmail.com>
2023-02-09 17:20:02 -0300

    eslint y prettier config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,33 @@
+// module.exports = {
+//   env: {
+//     browser: true,
+//     es2021: true,
+//     node: true
+//   },
+//   extends: 'standard',
+//   overrides: [
+//   ],
+//   parserOptions: {
+//     ecmaVersion: 'latest',
+//     sourceType: 'module'
+//   },
+//   rules: {
+//   }
+// }
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true
-  },
-  extends: 'standard',
-  overrides: [
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  rules: {
-  }
+	// ...
+	extends: ['standard', 'plugin:astro/recommended'],
+	rules: {
+		'no-tabs': 'off',
+		indent: 'off',
+		'space-before-function-paren': 'off'
+	},
+	// ...
+	overrides: [
+		{
+			files: ['*.astro'],
+			parser: 'astro-eslint-parser',
+			rules: {}
+		}
+	]
 }
