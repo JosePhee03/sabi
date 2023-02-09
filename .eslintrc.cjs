19dcc23d34aedef4e76ad7eedd7a4dae4ea896ab
JosePhee03 <tyttocr@gmail.com>
2023-02-09 19:28:19 -0300

    add components, imgs, svg and formated style

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,3 @@
-// module.exports = {
-//   env: {
-//     browser: true,
-//     es2021: true,
-//     node: true
-//   },
-//   extends: 'standard',
-//   overrides: [
-//   ],
-//   parserOptions: {
-//     ecmaVersion: 'latest',
-//     sourceType: 'module'
-//   },
-//   rules: {
-//   }
-// }
 module.exports = {
 	// ...
 	extends: ['standard', 'plugin:astro/recommended'],
@@ -27,6 +11,10 @@ module.exports = {
 		{
 			files: ['*.astro'],
 			parser: 'astro-eslint-parser',
+			parserOptions: {
+				parser: '@typescript-eslint/parser',
+				extraFileExtensions: ['.astro']
+			},
 			rules: {}
 		}
 	]
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  theme: {
-    extend: {}
-  },
-  plugins: []
+	darkMode: 'class',
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				primary: '#574EB1',
+				secondary: '#0656D0',
+				'light-gray': '#eeeeee',
+				'dark-gray': '#424864',
+				dark: '#292D3E',
+				'dark-100': '#272B3B',
+				'white-100': '#F9F9F9',
+				'semi-transparent': '#00000020'
+			},
+			fontFamily: {
+				nunito: ['"Nunito"', 'sans-serif'],
+				blinker: ['"Blinker"', 'sans-serif']
+			},
+			backgroundImage: {
+				sabi: 'linear-gradient(0deg, rgba(6,86,208,1) 0%, rgba(87,78,177,1) 100%)',
+				'bici-sabi': 'url("/img/bici-SABI.png")',
+				'luces-sabi': 'url("/img/luces-SABI.png")',
+				'control-sabi': 'url("/img/control-SABI.png")'
+			},
+			dropShadow: {
+				secondary: '0px 0px 5px #0656D050',
+				normal: '0px 0px 5px #00000010'
+			},
+			screens: {
+				mini: '360px'
+			},
+			fontSize: {
+				'6xl': '[3.75rem, { lineHeight: 1.1 }]'
+			}
+		}
+	},
+	plugins: []
 }
