75f3f615330ff28bd9340be8ec101ea2e93324d4
JosePhee03 <tyttocr@gmail.com>
2023-02-17 15:16:09 -0300

    add new icons

---
+++ src/components/icons.jsx
@@ -0,0 +1,52 @@
+export function PauseIcon() {
+	return (
+		<svg width='80' height='80' viewBox='0 0 80 80' fill='none' xmlns='http://www.w3.org/2000/svg'>
+			<rect width='80' height='80' rx='40' fill='black' fill-opacity='0.2' />
+			<path
+				d='M22 22.8571C22 21.2792 23.2792 20 24.8571 20H33.4286C35.0065 20 36.2857 21.2792 36.2857 22.8571V57.1429C36.2857 58.7208 35.0065 60 33.4286 60H24.8571C23.2792 60 22 58.7208 22 57.1429V22.8571Z'
+				fill='black'
+				fill-opacity='0.5'
+			/>
+			<path
+				d='M44 22.8571C44 21.2792 45.2792 20 46.8571 20H55.4286C57.0065 20 58.2857 21.2792 58.2857 22.8571V57.1429C58.2857 58.7208 57.0065 60 55.4286 60H46.8571C45.2792 60 44 58.7208 44 57.1429V22.8571Z'
+				fill='black'
+				fill-opacity='0.5'
+			/>
+		</svg>
+	)
+}
+
+export function PlayIcon() {
+	return (
+		<svg width='80' height='80' viewBox='0 0 80 80' fill='none' xmlns='http://www.w3.org/2000/svg'>
+			<rect width='80' height='80' rx='40' fill='black' fill-opacity='0.2' />
+			<path
+				d='M58.1463 42.3329C59.811 41.2231 59.811 38.7769 58.1463 37.6671L32.3591 20.4756C30.4958 19.2334 28 20.5691 28 22.8085V57.1915C28 59.4309 30.4958 60.7666 32.3591 59.5244L58.1463 42.3329Z'
+				fill='black'
+				fill-opacity='0.5'
+			/>
+		</svg>
+	)
+}
+
+export function ArrowDown() {
+	return (
+		<svg
+			viewBox='0 0 1024 1024'
+			class='animate-pulse'
+			version='1.1'
+			width='30'
+			height='30'
+			xmlns='http://www.w3.org/2000/svg'
+			fill='#000000'
+		>
+			<g id='SVGRepo_tracerCarrier' stroke-linecap='round' stroke-linejoin='round'>
+				<path
+					class='fill-text-color'
+					d='M903.232 256l56.768 50.432L512 768 64 306.432 120.768 256 512 659.072z'
+					fill='#000000'
+				></path>
+			</g>
+		</svg>
+	)
+}
